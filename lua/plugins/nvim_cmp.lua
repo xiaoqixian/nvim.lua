d2827f4f09f953948c188c30802a417610e1bdcd
Kaleidopink <lunar_ubuntu@qq.com>
2024-03-11 00:01:46 +0800

    repaired vsnip

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -1,20 +1,91 @@
 -- nvim-cmp config file
 local M = {}
 
+local function lsp_config()
+  local lspconfig = require('lspconfig')
+
+  local border = {
+    {"╭", "FloatBorder"},
+    {"─", "FloatBorder"},
+    {"╮", "FloatBorder"},
+    {"│", "FloatBorder"},
+    {"╯", "FloatBorder"},
+    {"─", "FloatBorder"},
+    {"╰", "FloatBorder"},
+    {"│", "FloatBorder"},
+  }
+
+  local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
+
+  -- config vim.diagnostic
+  vim.diagnostic.config({
+    virtual_text = false,
+    underline = true,
+    signs = {
+      active = signs
+    },
+    float = {
+      header = false,
+      border = "rounded",
+      focusable = false
+    }
+  })
+
+  -- show diagnostic after cursor 
+  -- holding for 500ms.
+  vim.opt.updatetime = 500
+
+  -- LSP settings (for overriding per client)
+  local handlers =  {
+    ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "rounded"}),
+    ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
+  }
+
+  local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+
+  for _, lsp in ipairs(servers) do
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    if lsp == "clangd" then
+      capabilities.semanticTokensProvider = nil
+    end
+    lspconfig[lsp].setup {
+      -- on_attach = my_custom_on_attach,
+      capabilities = capabilities,
+      handlers = handlers
+    }
+  end
+end
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 function M.init() 
   vim.g.nvim_cmp_exists = true
 
   local cmp = require("cmp")
   local lspkind = require("lspkind")
-  local luasnip = require("luasnip")
+  -- local luasnip = require("luasnip")
   --local vsnip = require("vsnip")
+
+  lsp_config()
 
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-         luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+         -- luasnip.lsp_expand(args.body) -- For `luasnip` users.
         -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
         -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       end,
@@ -29,6 +100,10 @@ function M.init()
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
+        elseif vim.fn["vsnip#available"](1) == 1 then
+          feedkey("<Plug>(vsnip-expand-or-jump)", "")
+        elseif has_words_before() then
+          cmp.complete()
         --[[ elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump() ]]
         --elseif vim.fn["vsnip#jumpable"](1) then
@@ -41,8 +116,10 @@ function M.init()
       ['<S-Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
+        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+          feedkey("<Plug>(vsnip-jump-prev)", "")
+        -- elseif luasnip.jumpable(-1) then
+        --   luasnip.jump(-1)
         --elseif vim.fn["vsnip#jumpable"](-1) then
           --vim.fn["vsnip#vsnip-jump-prev"]()
         else
@@ -52,8 +129,8 @@ function M.init()
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      --{ name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
       -- { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     }, {
