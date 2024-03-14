1740ac7f35268281c7c1c5b3680ac68e77287e97
Kaleidopink <lunar_ubuntu@qq.com>
2024-03-14 12:17:10 +0800

    update autocmd FileType shiftwidth settings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,9 +10,9 @@ vim.api.nvim_create_autocmd("BufNewFile", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "lua", "json", "vim", "xml", "html", "css", "typst" },
   callback = function()
-    vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
-    vim.opt.softtabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
   end
 })
 
