67f1b14ba1a2861dcee852361fb7fe0bc72c8407
Kaleidopink <lunar_ubuntu@qq.com>
2024-03-16 22:10:54 +0800

    fterm decide if startinsert by last left mode

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,29 +1,38 @@
 -- The config file for FTerm.nvim plugin
 local M = {}
+local opts = require("utils").keymap_opts
+local fterm_open = false
+local leave_in_insert_mode = true
 
-local function opts(desc)
-  return {
-    desc = desc,
-    silent = true,
-    nowait = true,
-    noremap = true
-  }
+local function fterm_toggle()
+  local fterm = require("FTerm")
+  if fterm_open then
+    leave_in_insert_mode = vim.fn.mode() ~= "n"
+    fterm.close()
+  else 
+    fterm.open()
+    if leave_in_insert_mode then
+      vim.cmd("startinsert")
+    end
+  end
+  fterm_open = not fterm_open
 end
+
 
 local function fterm_open()
   local fterm = require("FTerm")
   if not vim.g.is_fterm_open then
     vim.g.is_fterm_open = true
-    vim.keymap.set({ "n", "t" }, "zz", fterm.toggle, opts("toggle fterm"))
-    fterm.open()
+    vim.keymap.set({ "n", "t" }, "Z", fterm_toggle, opts("toggle fterm"))
+    fterm_toggle()
   else 
-    fterm.toggle()
+    fterm_toggle()
   end
 end
 
 function M.init() 
   vim.g.is_fterm_open = false
-  vim.keymap.set("n", "zz", fterm_open, opts("open fterm"))
+  vim.keymap.set("n", "Z", fterm_open, opts("open fterm"))
 end
 
 return M
