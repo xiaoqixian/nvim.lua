d47d1e66849f2a63d5dec47dc68f57c5442b6529
Kaleidopink <lunar_ubuntu@qq.com>
2024-03-17 19:37:14 +0800

    add telescope layout config | add some telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,10 +17,15 @@ local function set_keymaps()
   local map = vim.keymap.set
   local builtin = require("telescope.builtin")
 
-  map("n", "<leader>rg", builtin.live_grep, opts("telescope live_grep"))
   map("n", "<leader>tb", builtin.buffers, opts("telescope buffers"))
   map("n", "<leader>ss", builtin.lsp_document_symbols, opts("telescope lsp_document_symbols"))
   map("n", "<leader>td", builtin.diagnostics, opts("telescope diagnostics"))
+
+  map("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts("telescope goto definitions"))
+
+  map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts("telescope find files"))
+
+  map("n", "<leader>lg", "<cmd>Telescope live_grep", opts("telescope live grep"))
 end
 
 function M.init() 
@@ -33,6 +38,22 @@ function M.init()
       mappings = {
         n = {
           ["q"] = actions.close
+        }
+      },
+
+      layout_strategy = "vertical",
+      layout_config = {
+        vertical = {
+          height = 0.9,
+          preview_cutoff = 0.5,
+          prompt_position = "bottom",
+          width = 0.8
+        },
+        horizontal = {
+          height = 0.9,
+          preview_cutoff = 0.6,
+          prompt_position = "bottom",
+          width = 0.8
         }
       }
     },
