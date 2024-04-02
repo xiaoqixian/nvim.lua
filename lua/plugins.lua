58eb44de3840116b33569ca7c62ebc0e07c9d067
Kaleidopink <lunar_ubuntu@qq.com>
2024-04-02 12:03:51 +0800

    add go syntax highlighting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -174,6 +174,25 @@ require("lazy").setup({
     build = "./install.sh",
     init = require("plugins/silicon").init,
     enabled = false
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+
+  {
+    "charlespascoe/vim-go-syntax"
   }
 }, {
   install = {
