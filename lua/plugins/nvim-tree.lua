712a6654181eaad7a729b855fc22bef0a4635249
lunar <2100125584@qq.com>
2024-08-18 17:25:23 +0800

    set nvim-tree float bg to none

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,28 +2,13 @@ local M = {}
 
 local toggle_key = "E"
 
-local function smart_toggle()
-  if not vim.g.is_nvim_tree_exists then
-    return
-  end
-
-  local api = require("nvim-tree.api")
-
-  if not vim.g.is_nvim_tree_open then
-    vim.g.is_nvim_tree_open = true
-    api.tree.open()
-  else
-    api.tree.toggle()
-  end
-end
-
 local function toggle_or_focus()
   local api = require("nvim-tree.api")
   local currentBuf = vim.api.nvim_get_current_buf()
   local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
   if currentBufFt == "NvimTree" then
     api.tree.toggle()
-  else 
+  else
     api.tree.focus()
   end
 end
@@ -32,7 +17,7 @@ end
 local function on_attach(bufnr)
 
   local function opts(desc)
-    return { 
+    return {
       desc = "nvim-tree: " .. desc,
       buffer = bufnr,
       noremap = true,
@@ -56,7 +41,7 @@ local function on_attach(bufnr)
   vim.keymap.set("n", "s", api.node.open.vertical, opts("nvim-tree: vertical split view"))
   vim.keymap.set("n", "i", api.node.open.horizontal, opts("nvim-tree: horizontal split view"))
 
-  vim.keymap.set("n", ">", function() 
+  vim.keymap.set("n", ">", function()
     api.tree.change_root_to_node()
     vim.api.nvim_win_set_cursor(0, { 1, 0 })
   end, opts("nvim-tree: change root to node"))
@@ -73,7 +58,7 @@ local function on_attach(bufnr)
 
   -- macOS only
   if vim.loop.os_uname().sysname == "Darwin" then
-    vim.keymap.set("n", "o", function(node, ...)
+    vim.keymap.set("n", "o", function(node)
       local lib = require("nvim-tree.lib")
       node = node or lib.get_node_at_cursor()
       vim.fn.jobstart({"open", node.absolute_path}, {detach = true})
@@ -110,7 +95,7 @@ local function get_float_view_config()
 end
 
 -- init is always executed on startup
-function M.init() 
+function M.init()
   local float_view = get_float_view_config()
   local screen_width = vim.opt.columns:get()
   float_view.enable = screen_width <= 100
@@ -139,16 +124,18 @@ function M.init()
   vim.g.is_nvim_tree_exists = true
   vim.g.is_nvim_tree_open = false
   vim.keymap.set("n", toggle_key, require("nvim-tree.api").tree.open, { desc = "nvim-tree: toggle", noremap = true, silent = true, nowait = true })
- 
+
   -- auto adjust window width to select 
   -- open aside or open float.
   vim.api.nvim_create_autocmd("VimResized", {
     callback = function()
       local view = require("nvim-tree.view")
-      local screen_width = vim.opt.columns:get()
-      view.View.float.enable = screen_width <= 100
+      local new_screen_width = vim.opt.columns:get()
+      view.View.float.enable = new_screen_width <= 100
     end
   })
+
+  vim.cmd("hi NvimTreeNormalFloat ctermbg=none")
 end
 
 return M
