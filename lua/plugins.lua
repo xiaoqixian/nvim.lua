643585599813a53e8949045eb5bbbafa39a32506
Kaleidopink <lunar_ubuntu@qq.com>
2024-09-13 16:10:19 +0800

    disable typst in treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,7 +27,11 @@ require("lazy").setup({
         },
         highlight = {
          enable = true,
+<<<<<<< HEAD
          disable = { "vue", "json", "css", "html", "cmake" }
+=======
+         disable = { "vue", "json", "css", "html", "typst" }
+>>>>>>> 21d8533 (disable typst in treesitter)
         },
       })
     end
