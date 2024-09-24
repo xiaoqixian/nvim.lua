7d79037629e2407c7a16ab30f6cbebed9a87d2f2
Kaleidopink <lunar_ubuntu@qq.com>
2024-09-24 16:37:35 +0800

    remove select=true

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -35,7 +35,7 @@ function M.init()
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<CR>'] = cmp.mapping.confirm(),
 
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
