5cbdff33f609a14b2d0bb328bed70c6402f47d4a
lunar <2100125584@qq.com>
2024-11-11 11:46:26 +0800

    linux specific copy to clipboard

--- lua/config/machine_specific.lua
+++ lua/config/machine_specific.lua
@@ -2,3 +2,10 @@
 -- Mail:   lunar_ubuntu@qq.com
 -- Author: https://github.com/xiaoqixian
 
+local utils = require("utils")
+local opts = utils.keymap_opts
+local os_type = utils.os()
+
+if os_type == "Linux" then
+  vim.keymap.set("v", "<leader>y", ":'<,'>w! /tmp/clip<CR>", opts("linux copy to clipboard"))
+end
