50ccb70df3401ea46b71d178b839dc0af03498b1
lunar <lunar_ubuntu@qq.com>
2025-03-24 14:30:02 +0800

    neovim-session-manager setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -311,7 +311,25 @@ local plugins = {
   {
     "Shatur/neovim-session-manager",
     config = function()
-      require("session_manager").setup({})
+      local config = require("session_manager.config")
+      require("session_manager").setup({
+        autoload_mode = {
+          config.AutoloadMode.GitSession,
+          config.AutoloadMode.CurrentDir,
+          config.AutoloadMode.Disabled
+        }
+      })
+      vim.api.nvim_create_autocmd({ "VimLeave" }, {
+        callback = function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            -- Don't save while there's any 'nofile' buffer open.
+            if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
+              return
+            end
+          end
+          require("session_manager").save_current_session()
+        end
+      })
     end
   }
 }
