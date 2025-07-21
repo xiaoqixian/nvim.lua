4e6508f99df70e8e8aee239225af92d4220df1b6
lunar <lunar_ubuntu@qq.com>
2025-07-21 19:01:03 +0800

    vim.diagnostics.config still have some problems, bring back sign_define(), stay in nvim v0.10.4

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -18,30 +18,35 @@ function M.init()
     {"│", "FloatBorder"},
   }
 
+  local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
   -- config vim.diagnostic
   vim.diagnostic.config({
     virtual_text = false,
     underline = true,
     signs = {
-      -- active = signs,
-      text = {
-        [vim.diagnostic.severity.ERROR] = "",
-        [vim.diagnostic.severity.WARN] = "",
-        [vim.diagnostic.severity.HINT] = "󰌵",
-        [vim.diagnostic.severity.INFO] = "",
-      },
-      texthl = {
-        [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-        [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-        [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-        [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-      },
-      numhl = {
-        [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-        [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-        [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-        [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-      },
+      active = signs,
+      -- text = {
+      --   [vim.diagnostic.severity.ERROR] = "",
+      --   [vim.diagnostic.severity.WARN] = "",
+      --   [vim.diagnostic.severity.HINT] = "󰌵",
+      --   [vim.diagnostic.severity.INFO] = "",
+      -- },
+      -- texthl = {
+      --   [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      --   [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      --   [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      --   [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+      -- },
+      -- numhl = {
+      --   [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      --   [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      --   [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      --   [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+      -- },
     },
     float = {
       header = false,
