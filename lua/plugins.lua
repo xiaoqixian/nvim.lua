17b2ccef612369f7a413a5528e347067c5d47451
lunar <2100125584@qq.com>
2026-07-10 23:20:44 +0800

    avante.nvim: reduced some redundant providers

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -366,24 +366,6 @@ local plugins = {
     opts = {
       provider = "openai",
       providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-sonnet-4-20250514",
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
-        },
-        moonshot = {
-          endpoint = "https://api.moonshot.ai/v1",
-          model = "kimi-k2-0711-preview",
-          timeout = 30000,
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 32768,
-          },
-        },
         openai = {
           endpoint = "https://api.deepseek.com",
           model = "deepseek-v4-flash",
